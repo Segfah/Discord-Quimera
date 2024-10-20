04c6090b274df2a7924659a3145d8d19a05f00ce
Segfah <42corozco@gmail.com>
2024-10-20 18:22:21 +0200

    Add printlogs command for displaying error and success logs

---
+++ commands/printlogs.js
@@ -0,0 +1,10 @@
+/*
+ Crear 2 funciones
+ 1) imprimir los ultimos 5 logs de error
+ 2) imprimir los ultimos 5 logs de éxito.
+ (opcional) Crear una función que imprima los logs deseados, con un argumento, ver ejemplo de uso
+
+ uso:
+    !printlogs error 2
+    !printlogs success 3
+*/
